57f3919bc9c4f9ed037af325e2df816767c140b4
Jura Gorohovsky <gorohoroh@gmail.com>
2024-04-15 01:46:10 +0600

    Seeding restaurants

---
+++ OdeToFood/Data/Migrations/20240414194341_SeedInitialRestaurants.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OdeToFood.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialRestaurants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Restaurants",
+                columns: new[] { "Id", "Cuisine", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The Italian Bistro" },
+                    { 2, 2, "French Delight" },
+                    { 3, 3, "Berlin's Best" },
+                    { 4, 1, "Mama Mia's" },
+                    { 5, 2, "The French Bakery" },
+                    { 6, 3, "German Goodness" },
+                    { 7, 1, "Pasta Paradise" },
+                    { 8, 2, "Parisian Pleasures" },
+                    { 9, 3, "Frankfurt Flavors" },
+                    { 10, 1, "Venetian Vittles" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
--- OdeToFood/ViewModels/HomeIndexViewModel.cs
+++ OdeToFood/ViewModels/HomeIndexViewModel.cs
@@ -5,6 +5,5 @@ namespace OdeToFood.ViewModels
     public class HomeIndexViewModel
     {
         public IEnumerable<Restaurant> Restaurants { get; set; }
-        public string CurrentMessage { get; set; }
     }
 }
